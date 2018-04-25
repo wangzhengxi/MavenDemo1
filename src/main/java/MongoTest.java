5558a0e911ac26f346553489b53b1c262560ea63
wangzhengxi <695310480@qq.com>
2018-04-25 19:15:35 +0800

    mongoDB

---
+++ src/main/java/MongoTest.java
@@ -0,0 +1,21 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Created by wangzhengxi on 2018/4/25.
+ */
+public class MongoTest {
+    public static void main( String args[] ){
+        try{
+            // 连接到 mongodb 服务
+            MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+            // 连接到数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("mycol");
+            System.out.println("Connect to database successfully");
+
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+}
