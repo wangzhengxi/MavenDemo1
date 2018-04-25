be45d87985350af3208db58b4618fb145e6f61e4
wangzhengxi <695310480@qq.com>
2018-04-25 18:50:37 +0800

    测试

---
+++ src/main/java/TestMethod.java
@@ -0,0 +1,9 @@
+/**
+ * Created by wangzhengxi on 2018/3/29.
+ */
+public class TestMethod {
+
+    public static void main(String[] args) {
+        System.out.println(("–"+ "boundary" + "\r\n").getBytes());
+    }
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>my name is jenkins11111111111</h2>
-</body>
-</html>
